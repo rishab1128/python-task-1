fcab62025fa0767125a41eea69a042c7cdb3ece7
rishab1128 <rishabpurkayastha28@gmail.com>
2023-04-20 13:09:57 +0530

    Both lists merged

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 list_1 = [
     {"id": "1", "name": "Shrey", "age": 25},
-    {"id": "3", "age": 10, "name": "Hello"},
     {"id": "2", "name": "World", "age": 24},
+    {"id": "3", "age": 10, "name": "Hello"},
 ]
 
 list_2 = [
@@ -18,15 +18,41 @@ list_2 = [
 
 
 def merge_lists(list_1, list_2) -> list:
-    """
-    Complete this function, by merging the information from list_1 and list_2
-    to create a new list, which has all the information about each student from
-    both lists in one single dict.
+    final_dict = {}   #Key = id , Value = Dictionary storing info like name,age,marks
+    merged_list = []
+    
+    for student_1 in list_1:
+        student_id = student_1["id"]
+        merged_student = student_1
+        
+        for student_2 in list_2:
+            if student_2.get("id") == student_id:
+                merged_student.update(student_2)
+                break
+        
+        id = merged_student['id']
+        del merged_student['id']
+        final_dict[id] = merged_student
+    
+    for student in list_2:
+        id = student.get("id")
+        if final_dict.get(id)==None:
+            del student['id']
+            final_dict[id] = student
 
-    - Both lists are unsorted
-    - Both lists can have missing values (for ex list_2 has missing id=2)
-    """
-    # return list_3
+    merged_list.append(final_dict)
+    
+    return merged_list
+
+
 
 
 list_3 = merge_lists(list_1, list_2)
+print (list_3)
+
+
+# Access all info about student_id = 1
+print(list_3[0]['1'])
+
+#Access particular info like name of student_id = 1
+print(list_3[0]['1']['name'])
